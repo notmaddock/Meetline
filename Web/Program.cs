2a81ca898075ad13438f2531a16b916a638cd7d3
Maddock <sowtag@gmail.com>
2026-03-13 04:14:12 -0300

    feat: JwtBearer authentication

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,4 +1,6 @@
 using Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Scalar.AspNetCore;
 using Web.Configs;
 using Web.Endpoints;
@@ -7,19 +9,34 @@ using Web.Endpoints.V1;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => { builder.Configuration.GetSection("JwtBearer").Bind(options); });
+
+builder.Services.AddAuthorizationBuilder()
+    .SetFallbackPolicy(new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build());
+
+builder.Services
     .AddInfrastructure()
     .AddOpenApi(OpenApiConfiguration.Configure)
     .AddApiVersioning(ApiVersioningConfiguration.Configure);
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.MapScalarApiReference();
 
-    app.MapGroup("/_debug").MapDebugV1Endpoints();
+    app.MapGroup("/_debug").AllowAnonymous().MapDebugV1Endpoints();
 }
+
+app.MapGet("/public", () => "welcome to a public endpoint").AllowAnonymous();
+app.MapGet("/private", () => "welcome to a private endpoint!");
 
 app.MapEndpoints();
 
