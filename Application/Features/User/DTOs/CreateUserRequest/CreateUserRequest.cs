49e3c66a9b6abbf77b67eeb4a6567425188308e8
Maddock <sowtag@gmail.com>
2026-04-05 06:10:02 -0300

    fix: make required fields required in CreateUserRequest.cs

--- Application/Features/User/DTOs/CreateUserRequest/CreateUserRequest.cs
+++ Application/Features/User/DTOs/CreateUserRequest/CreateUserRequest.cs
@@ -1,7 +1,11 @@
 namespace Application.Features.User.DTOs.CreateUserRequest;
 
-public record CreateUserRequest(
-    string Username,
-    string Email,
-    string? FirstName,
-    string? LastName);
+public record CreateUserRequest
+{
+    public required string Username { get; init; }
+    public required string Email { get; init; }
+
+    public string? FirstName { get; init; }
+
+    public string? LastName { get; init; }
+}
