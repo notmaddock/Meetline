bfbfff01d88d3fef66aa79d431bceca7d33a12c6
Maddock <sowtag@gmail.com>
2026-05-11 19:42:24 -0300

    feat: add pg with different databases to AppHost

--- src/Backend/Meetline.AppHost/AppHost.cs
+++ src/Backend/Meetline.AppHost/AppHost.cs
@@ -1,3 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder.AddPostgres("postgres-master")
+    .WithDataVolume();
+
+var usersPostgres = postgres.AddDatabase("postgres-users");
+var rolesPostgres = postgres.AddDatabase("postgres-roles");
+
+builder.AddProject<Projects.Web>("meetline-backend")
+    .WithReference(usersPostgres)
+    .WithReference(rolesPostgres)
+    .WaitFor(postgres);
+
 builder.Build().Run();
