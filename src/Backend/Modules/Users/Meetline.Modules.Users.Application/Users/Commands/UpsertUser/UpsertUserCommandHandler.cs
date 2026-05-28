a3a3a975af9e559552368edd065918a0ba35e9c8
Maddock <sowtag@gmail.com>
2026-05-28 16:10:12 -0300

    feat: add new events and handler when a user syncs from IdP

--- src/Backend/Modules/Users/Meetline.Modules.Users.Application/Users/Commands/UpsertUser/UpsertUserCommandHandler.cs
+++ src/Backend/Modules/Users/Meetline.Modules.Users.Application/Users/Commands/UpsertUser/UpsertUserCommandHandler.cs
@@ -1,13 +1,18 @@
 using Meetline.Modules.Users.Application.Repositories;
+using Meetline.Modules.Users.Application.Users.Events;
 using Meetline.Modules.Users.Application.Users.Mappers;
 
 namespace Meetline.Modules.Users.Application.Users.Commands.UpsertUser;
 
 public static class UpsertUserCommandHandler
 {
-    public static Task Handle(UpsertUserCommand command, IUserRepository userRepository, CancellationToken ct)
+    public static async Task<UserProfileSyncedEvent> Handle(
+        UpsertUserCommand command,
+        IUserRepository userRepository,
+        CancellationToken ct)
     {
         var user = UserMapper.ToEntity(command.Data);
-        return userRepository.UpsertByExternalIdAsync(user, ct);
+        var upsertedUser = await userRepository.UpsertByExternalIdAsync(user, ct);
+        return new UserProfileSyncedEvent(upsertedUser.Id, upsertedUser.ExternalId);
     }
 }
